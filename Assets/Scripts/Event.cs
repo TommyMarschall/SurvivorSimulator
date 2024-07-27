86fe1742680226078f5d15003465cbac090e622e
TommyMarschall <166242296+TommyMarschall@users.noreply.github.com>
2024-07-27 11:03:12 -0700

    Did stuff

---
+++ Assets/Scripts/Event.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class AllyEvent
+{
+    public float modifier;
+    public string discriptor;
+    public AllyEvent(float modifier, string discriptor)
+    {
+        this.modifier = modifier;
+        this.discriptor = discriptor;
+    }
+}
--- Assets/Scripts/Simulator.cs
+++ Assets/Scripts/Simulator.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 
 public class Simulator : MonoBehaviour
 {
+    public GameObject SimulationScreen;
     public List<Contestant> contestants;
     public List<Tribe> tribes;
     // Start is called before the first frame update
@@ -29,6 +31,8 @@ public class Simulator : MonoBehaviour
     }
     public void Simulate()
     {
+        GameObject.Find("StartScreen").SetActive(false);
+        SimulationScreen.SetActive(true);
         foreach (var Tribe in tribes)
         {
             foreach (var contestant in Tribe.TribeSelector.GetComponent<TribeSelector>().TribeSlots)
@@ -36,5 +40,17 @@ public class Simulator : MonoBehaviour
                 Tribe.tribeMembers.Add(contestants.Find(c => c.name == contestant.name));
             }
         }
+    }
+    public string[] posAllies = { "Find Something in Common","Bond Strongly","Bond Slightly",""};
+    public string[] negAllies = { "Have a Minor Disagreement","Has a Major Meltdown","Have a Small Fight","Have a Major Disagreement","Have a Major Fight"};
+    void PickEvent (Contestant C1 , Contestant C2)
+    {
+        float mod = Random.Range (-1.0f, 1.0f);
+        string desc;
+        if (mod > 0) 
+        { 
+            desc = posAllies[Random.next(0, posAllies.Length)];
+        }
+        AllyEvent AllyEvent = new AllyEvent (mod,)
     }
 }
