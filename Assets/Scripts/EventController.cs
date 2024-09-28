3de4b2ad12cf211973599fd0d57757b5889fb111
Zachary Davis <misterzd@icloud.com>
2024-09-28 13:01:08 -0500

    Zach's updated project files for sim

--- Assets/Scripts/EventController.cs
+++ Assets/Scripts/EventController.cs
@@ -1,43 +1,35 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class EventController : MonoBehaviour
 {
-    string player1;
-    string player2;
-    public TextMeshProUGUI textbox1;
-    public List<string> players;
-    public List<Image> playerProfiles;
-    public List<string> Pagong;
-    public List<string> Tagi;
-    public GameObject gameObject;
+    public string player1; // Name of player 1
+    public string player2; // Name of player 2
+    public TextMeshProUGUI textbox1; // Reference to the TextMeshProUGUI component
+    public List<string> players; // List of player names
+    public List<Image> playerProfiles; // List of player profile images
+    public List<string> Pagong; // List of players in Tribe Pagong
+    public List<string> Tagi; // List of players in Tribe Tagi
+    public GameObject eventPrefab; // Prefab for creating UI events
 
-    
     // Start is called before the first frame update
     void Start()
     {
-       /* for (int i = 0; i < 8; i++) {
-            Pagong.Add(players[i]);
-        }
-      
-        for (int i = 0; i < 8; i++)
-        {
-            Tagi.Add(players[i]);
-        }*/
-        player1 = "B.B";
-        player2 = "Colleen";
-        string alliance = players[Random.Range(0,15)] + " and " +  players[Random.Range(0,15)] + " formed an alliance";
+        player1 = "B.B";  // Initial player 1 name
+        player2 = "Colleen";  // Initial player 2 name
 
+        // Create a random alliance text using random players from the players list
+        string alliance = players[Random.Range(0, players.Count)] + " and " + players[Random.Range(0, players.Count)] + " formed an alliance";
+
+        // Set the alliance text in the textbox
         textbox1.text = alliance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Not used right now, but can be used for updates
     }
 }
--- Assets/Scripts/Simulator.cs
+++ Assets/Scripts/Simulator.cs
@@ -1,24 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 using TMPro;
 using UnityEngine.UI;
 
-
 public class Simulator : MonoBehaviour
 {
-    public GameObject SimulationScreen;
-    public List<Contestant> contestants;
-    public List<Tribe> tribes;
-    public GameObject EventPrefab;
-    public GameObject EventScreen;
-    // Start is called before the first frame update
-    private Button button;
-    
-    public void StartSimulation(){
+    public List<Contestant> contestants; // List of all contestants in the simulation
+    public List<Tribe> tribes; // List of all tribes in the simulation
+    public GameObject EventPrefab; // Prefab to display individual events
+
+    public string[] posAllies = { "Find Something in Common", "Bond Strongly", "Bond Slightly" };
+    public string[] negAllies = { "Have a Minor Disagreement", "Has a Major Meltdown", "Have a Small Fight", "Have a Major Disagreement", "Have a Major Fight" };
+
+    // Initialize relationships and simulate the events
+    public void StartSimulation()
+    {
         Debug.Log("Simulation Started");
 
+        // Create relationships between contestants
         foreach (var contestant in contestants)
         {
             foreach (var contestant2 in contestants)
@@ -29,56 +29,62 @@ public class Simulator : MonoBehaviour
                 }
             }
         }
+
         Simulate();
     }
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    // Runs the main simulation logic
     public void Simulate()
     {
-        Destroy(GameObject.Find("Event(Clone)"));
+        // Pick a random AllyEvent between two contestants in the first tribe
+        AllyEvent allyEvent = PickEvent(contestants[Random.Range(0, contestants.Count)], contestants[Random.Range(0, contestants.Count)]);
+        Debug.Log("Ally Event: " + allyEvent);
 
-        GameObject.Find("StartScreen").SetActive(false);
-        SimulationScreen.SetActive(true);
-        foreach (var Tribe in tribes)
-        {
-            foreach (var contestant in Tribe.TribeSelector.GetComponent<TribeSelector>().TribeSlots)
-            {
-                Tribe.tribeMembers.Add(contestants.Find(c => c.name == contestant.name));
-            }
-        }
-        AllyEvent AllyEvent = PickEvent(tribes[0].tribeMembers[new System.Random().Next(0,8)], tribes[0].tribeMembers[new System.Random().Next(0,8)]);
-        Debug.Log("Ally Event: " + AllyEvent);
-        CreateEventVisual(AllyEvent);
-       // EventScreen.SetActive(false);
-
-        
+        // Create a visual representation of the event
+        CreateEventVisual(allyEvent);
     }
-    public string[] posAllies = { "Find Something in Common","Bond Strongly","Bond Slightly"};
-    public string[] negAllies = { "Have a Minor Disagreement","Has a Major Meltdown","Have a Small Fight","Have a Major Disagreement","Have a Major Fight"};
-    AllyEvent PickEvent (Contestant C1 , Contestant C2)
+
+    // Picks a random event based on a relationship modifier
+    AllyEvent PickEvent(Contestant C1, Contestant C2)
     {
-        float mod = UnityEngine.Random.Range (-1.0f, 1.0f);
+        float mod = UnityEngine.Random.Range(-1.0f, 1.0f); // Random relationship modifier
         string desc;
-        if (mod > 0) 
-        { 
-            desc = posAllies[new System.Random().Next(0, posAllies.Length)];
-        } 
-        else { desc = negAllies[new System.Random().Next(0, negAllies.Length)]; } 
+
+        if (mod > 0)
+        {
+            desc = posAllies[Random.Range(0, posAllies.Length)];
+        }
+        else
+        {
+            desc = negAllies[Random.Range(0, negAllies.Length)];
+        }
+
         Debug.Log(desc);
-        AllyEvent AllyEvent = new AllyEvent(mod,desc,C1,C2);
-        Debug.Log(C1.name+" and " +C2.name+" "+AllyEvent.ToString());
-        return AllyEvent;
+
+        AllyEvent allyEvent = new AllyEvent(mod, desc, C1, C2);
+        Debug.Log(C1.name + " and " + C2.name + " " + allyEvent.ToString());
+
+        return allyEvent;
     }
-    void CreateEventVisual(AllyEvent AllyEvent)
+
+    // Creates a visual representation of the event in the root of the scene
+    void CreateEventVisual(AllyEvent allyEvent)
     {
-        GameObject event1 = Instantiate(EventPrefab);
-        event1.GetComponent<RectTransform>().SetParent(SimulationScreen.GetComponent<RectTransform>());
+        GameObject event1 = Instantiate(EventPrefab); // Instantiate the EventPrefab in the root of the scene
+        event1.transform.SetParent(null); // This ensures it is not a child of any UI element
+
+        // Set the position of the event UI element (adjust as necessary)
+        event1.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, 0, 0); // Centered position
+
+        // Set the scale to keep it properly sized
         event1.transform.localScale = Vector3.one;
-        event1.transform.GetChild(0).GetComponent<TMP_Text>().text = AllyEvent.contestant1.name + " and " + AllyEvent.contestant2.name + " " + AllyEvent.ToString();
-        event1.transform.GetChild(1).GetComponent<Image>().sprite = AllyEvent.contestant1.Image.GetComponent<Image>().sprite;
-        event1.transform.GetChild(2).GetComponent<Image>().sprite = AllyEvent.contestant2.Image.GetComponent<Image>().sprite;
+
+        // Display event details on the UI
+        event1.transform.GetChild(0).GetComponent<TMP_Text>().text = allyEvent.contestant1.name + " and " + allyEvent.contestant2.name + " " + allyEvent.ToString();
+
+        // Access the Image component and set the sprite
+        event1.transform.GetChild(1).GetComponent<Image>().sprite = allyEvent.contestant1.Image.GetComponent<Image>().sprite;
+        event1.transform.GetChild(2).GetComponent<Image>().sprite = allyEvent.contestant2.Image.GetComponent<Image>().sprite;
     }
+
 }
