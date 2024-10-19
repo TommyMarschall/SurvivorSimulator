b96520ebad2eb61401e9ef1690083ff5ddefc9c7
Zachary Davis <misterzd@icloud.com>
2024-10-19 12:23:22 -0500

    updated merge conflict

--- Assets/Scripts/Simulator.cs
+++ Assets/Scripts/Simulator.cs
@@ -5,34 +5,25 @@ using System;
 using TMPro;
 using UnityEngine.UI;
 
-
 public class Simulator : MonoBehaviour
 {
-<<<<<<< Updated upstream
-    public GameObject SimulationScreen;
-    public List<Contestant> contestants;
-    public List<Tribe> tribes;
-    public GameObject EventPrefab;
-    public GameObject EventScreen;
-    // Start is called before the first frame update
-    private Button button;
-    //Initalizes the simulation by setting up relationships between contestants
-    public void StartSimulation(){
-=======
-    public GameObject SimulatorScreen;
+    public GameObject SimulatorScreen; // Fixed naming inconsistency from "SimulationScreen"
     public List<Contestant> contestants; // List of all contestants in the simulation
     public List<Tribe> tribes; // List of all tribes in the simulation
     public GameObject EventPrefab; // Prefab to display individual events
+    public GameObject EventScreen;
+    public Button button; // Added the button reference for consistency
 
+    // List of potential positive and negative events
     public string[] posAllies = { "Find Something in Common", "Bond Strongly", "Bond Slightly" };
     public string[] negAllies = { "Have a Minor Disagreement", "Has a Major Meltdown", "Have a Small Fight", "Have a Major Disagreement", "Have a Major Fight" };
 
-    // Initialize relationships and simulate the events
+    // Start is called before the first frame update
     public void StartSimulation()
     {
->>>>>>> Stashed changes
         Debug.Log("Simulation Started");
 
+        // Initialize relationships between contestants
         foreach (var contestant in contestants)
         {
             foreach (var contestant2 in contestants)
@@ -45,65 +36,66 @@ public class Simulator : MonoBehaviour
         }
         Simulate();
     }
-    //Runs the main Simulation
+
+    // Runs the main Simulation
     public void Simulate()
     {
-        //Destroy any existing screen and hide the start screen
+        // Destroy any existing event screen and hide the start screen
         Destroy(GameObject.Find("Event(Clone)"));
-        //Shows Simulation screen and hides start screen
+        
+        // Show Simulation screen and hide start screen
         GameObject.Find("StartScreen").SetActive(false);
-        SimulationScreen.SetActive(true);
-        //assign contestants to their tribe
-        foreach (var Tribe in tribes)
+        SimulatorScreen.SetActive(true);
+        
+        // Assign contestants to their tribe
+        foreach (var tribe in tribes)
         {
-            foreach (var contestant in Tribe.TribeSelector.GetComponent<TribeSelector>().TribeSlots)
+            foreach (var contestant in tribe.TribeSelector.GetComponent<TribeSelector>().TribeSlots)
             {
-                Tribe.tribeMembers.Add(contestants.Find(c => c.name == contestant.name));
+                tribe.tribeMembers.Add(contestants.Find(c => c.name == contestant.name));
             }
         }
-        //Pick random event for two people
-        AllyEvent AllyEvent = PickEvent(tribes[0].tribeMembers[new System.Random().Next(0,8)], tribes[0].tribeMembers[new System.Random().Next(0,8)]);
-        Debug.Log("Ally Event: " + AllyEvent);
-        CreateEventVisual(AllyEvent);
 
-        
+        // Pick a random event for two people
+        AllyEvent allyEvent = PickEvent(tribes[0].tribeMembers[new System.Random().Next(0, 8)], tribes[0].tribeMembers[new System.Random().Next(0, 8)]);
+        Debug.Log("Ally Event: " + allyEvent);
+        CreateEventVisual(allyEvent);
     }
-    //List of potential positive and negative events
-    public string[] posAllies = { "Find Something in Common","Bond Strongly","Bond Slightly"};
-    public string[] negAllies = { "Have a Minor Disagreement","Has a Major Meltdown","Have a Small Fight","Have a Major Disagreement","Have a Major Fight"};
-    AllyEvent PickEvent (Contestant C1 , Contestant C2)
+
+    // Picks an ally event between two contestants
+    AllyEvent PickEvent(Contestant C1, Contestant C2)
     {
-        float mod = UnityEngine.Random.Range (-1.0f, 1.0f);
+        float mod = UnityEngine.Random.Range(-1.0f, 1.0f);
         string desc;
-        if (mod > 0) 
-        { 
+
+        if (mod > 0)
+        {
             desc = posAllies[new System.Random().Next(0, posAllies.Length)];
-        } 
-        else { desc = negAllies[new System.Random().Next(0, negAllies.Length)]; } 
+        }
+        else
+        {
+            desc = negAllies[new System.Random().Next(0, negAllies.Length)];
+        }
+
         Debug.Log(desc);
-        AllyEvent AllyEvent = new AllyEvent(mod,desc,C1,C2);
-        Debug.Log(C1.name+" and " +C2.name+" "+AllyEvent.ToString());
-        return AllyEvent;
+        AllyEvent allyEvent = new AllyEvent(mod, desc, C1, C2);
+        Debug.Log(C1.name + " and " + C2.name + " " + allyEvent.ToString());
+        return allyEvent;
     }
-    void CreateEventVisual(AllyEvent AllyEvent)
+
+    // Creates a visual representation of the event
+    void CreateEventVisual(AllyEvent allyEvent)
     {
-<<<<<<< Updated upstream
-        GameObject event1 = Instantiate(EventPrefab);
-        event1.transform.SetParent(null);
-        event1.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, 0, 0);
-=======
-        GameObject event1 = Instantiate(EventPrefab,SimulatorScreen.transform); // Instantiate the EventPrefab in the root of the scene
-        
+        // Instantiate the EventPrefab in the root of the scene or specific screen
+        GameObject event1 = Instantiate(EventPrefab, SimulatorScreen.transform);
 
         // Set the position of the event UI element (adjust as necessary)
         event1.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, 0, 0); // Centered position
 
         // Set the scale to keep it properly sized
->>>>>>> Stashed changes
         event1.transform.localScale = Vector3.one;
-        event1.transform.GetChild(0).GetComponent<TMP_Text>().text = AllyEvent.contestant1.name + " and " + AllyEvent.contestant2.name + " " + AllyEvent.ToString();
-        event1.transform.GetChild(1).GetComponent<Image>().sprite = AllyEvent.contestant1.Image.GetComponent<Image>().sprite;
-        event1.transform.GetChild(2).GetComponent<Image>().sprite = AllyEvent.contestant2.Image.GetComponent<Image>().sprite;
+        event1.transform.GetChild(0).GetComponent<TMP_Text>().text = allyEvent.contestant1.name + " and " + allyEvent.contestant2.name + " " + allyEvent.ToString();
+        event1.transform.GetChild(1).GetComponent<Image>().sprite = allyEvent.contestant1.Image.GetComponent<Image>().sprite;
+        event1.transform.GetChild(2).GetComponent<Image>().sprite = allyEvent.contestant2.Image.GetComponent<Image>().sprite;
     }
-
 }
